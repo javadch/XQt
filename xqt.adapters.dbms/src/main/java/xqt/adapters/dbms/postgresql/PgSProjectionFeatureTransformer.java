134f9c88a1d78c439bcbe16f2e5ca23bcf6c3311
javadch <javad.chamanara@uni-jean.de>
2016-04-11 14:26:34 +0200

    Added base classes for the query transformation services

---
+++ xqt.adapters.dbms/src/main/java/xqt/adapters/dbms/postgresql/PgSProjectionFeatureTransformer.java
@@ -0,0 +1,15 @@
+package xqt.adapters.dbms.postgresql;
+
+import java.util.Map;
+
+import xqt.model.transformation.QueryFeatureTransformer;
+
+public class PgSProjectionFeatureTransformer implements QueryFeatureTransformer{
+
+	@Override
+	public String transform(Object feature, Map<String, Object> queryFeatures) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
