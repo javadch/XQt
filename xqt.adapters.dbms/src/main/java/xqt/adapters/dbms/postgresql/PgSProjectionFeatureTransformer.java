f0e0470f21f6afd936e8de5d7fab4630c4523cb8
javadch <chamanara@gmail.com>
2016-04-26 14:27:40 +0200

    Skeleton class for the PostgreSQL projection feature transformer

--- xqt.adapters.dbms/src/main/java/xqt/adapters/dbms/postgresql/PgSProjectionFeatureTransformer.java
+++ xqt.adapters.dbms/src/main/java/xqt/adapters/dbms/postgresql/PgSProjectionFeatureTransformer.java
@@ -2,6 +2,8 @@ package xqt.adapters.dbms.postgresql;
 
 import java.util.Map;
 
+import xqt.model.declarations.PerspectiveAttributeDescriptor;
+import xqt.model.statements.query.ProjectionFeature;
 import xqt.model.transformation.QueryFeatureTransformer;
 
 public class PgSProjectionFeatureTransformer implements QueryFeatureTransformer{
@@ -9,7 +11,13 @@ public class PgSProjectionFeatureTransformer implements QueryFeatureTransformer{
 	@Override
 	public String transform(Object feature, Map<String, Object> queryFeatures) {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder(); 
+		ProjectionFeature projection = (ProjectionFeature)feature;
+		for(PerspectiveAttributeDescriptor attribute: projection.getPerspective().getAttributes().values()){
+			//attribute.
+			sb.append("Hi");
+		}
+		return sb.toString();
 	}
 
 }
