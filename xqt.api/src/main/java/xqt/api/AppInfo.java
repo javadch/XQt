cd2a2c4cca8bab03a55cf7cdfbc0a34aca9973c1
javadch <chamanara@gmail.com>
2016-05-24 15:21:57 +0200

    increased API version to 0.4.0

--- xqt.api/src/main/java/xqt/api/AppInfo.java
+++ xqt.api/src/main/java/xqt/api/AppInfo.java
@@ -13,7 +13,7 @@ import xqt.model.adapters.AdapterInfoContainer;
 public class AppInfo {
     public static String getVersion(){
         // grab this information from a config file that is used by other parts of the software too. DRY please.
-        return "0.3.0"; 
+        return "0.4.0"; 
     }
     
     public static String getMaturityLevel(){
@@ -21,7 +21,7 @@ public class AppInfo {
     }
     
     public static String getName(){
-        return "XQt Language";
+        return "QUIS Language";
     }
     
     public static String getFullName(){
